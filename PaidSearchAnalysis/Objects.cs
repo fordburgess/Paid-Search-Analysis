5819cde33ab0a67b23c9e63da7e3c26e54196f89
Ford Burgess <76268861+fordburgess@users.noreply.github.com>
2022-05-29 10:57:19 -0500

    first commit no errors

---
+++ PaidSearchAnalysis/Objects.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaidSearchAnalysis
+{
+
+    public class BrandData
+    {
+        public string Brand { get; set; }
+        public double SearchesSum { get; set; }
+        public double ImpressionsSum { get; set; }
+        public double NetCostSum { get; set; }
+    }
+
+    public class Output
+    {
+        public string YearHalf { get; set; }
+        public List<BrandData> Outputs { get; set; }
+        public double SearchesTotal { get; set; }
+        public double ImpressionsTotal { get; set; }
+        public double NetCostTotal { get; set; }
+
+    }
+}
